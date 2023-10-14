f1bede34e4b33124ad596019d840f74862b3af11
Wilson <wilson.diant@gmail.com>
2023-10-13 22:27:43 -0400

    added search

--- natives/__init__.py
+++ natives/__init__.py
@@ -2,11 +2,15 @@
 from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:////Users/wilson/Desktop/native/VD.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
--- natives/route.py
+++ natives/route.py
@@ -9,6 +9,7 @@ from natives import app
 from natives.models import *
 from natives.parsing import *
 
+from flask_cors import cross_origin
 
 db.init_app(app)
 
@@ -16,78 +17,103 @@ voyceSchemas = Voyce_Schema(many=True)
 voyceSchema = Voyce_Schema()
 
 
-@app.route('/read', methods=['GET'])
+@app.route('/read', methods=['GET','POST'])
+
 def index():
+    if request.method == 'POST':
+        searchquery =  request.json["serial"]
 
-    data = Voyce.query.all()
-    out = voyceSchemas.dump(data)
-    response = jsonify(out)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+        if searchquery:
+            results = Voyce.query.filter(Voyce.SN.icontains(searchquery) ).all()
+        else:
+            results =  Voyce.query.all()
+        out = voyceSchemas.dump(results)
+        response = jsonify(out)
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+    
 
 
-@app.route('/create', methods=['GET'])
+@app.route('/create', methods=['POST','GET'])
 def create():
-    try:
-        # cmd = 'cfgutil -f --format JSON get name serialNumber wifiAddress'
-        # result = subprocess.run(
-        #     cmd, shell=True, capture_output=True, text=True, check=True)
-        # output = json.loads(str(result.stdout))
-        output = {
-            "Command": "get",
-            "Output": {
-                "0x954A00E7B0026": {
-                    "name": "ATEST 001",
-                    "wifiAddress": "c4:2a:d0:5f:ca:b0",
-                    "serialNumber": "F9FZ5J5KMF3M"
-                },
-                "0x1459941A41402E": {
-                    "name": "ATEST 002",
-                    "wifiAddress": "4c:b9:10:97:e9:ed",
-                    "serialNumber": "GG7FFKTQQ1GC"
-                },  
-                "0x1459FR41A41402E": {
-                    "name": "ATEST 03",
-                    "wifiAddress": "4c:b9:10:97:e9:ed",
-                    "serialNumber": "GG7FFKTQQ1GC"
-                },
-                "Errors": {
-                    "0x1459941A41402E": {},
-                    "0x954A00E7B0026": {}
-                }
-            },
-            
-            "Type": "CommandOutput",
-            "Devices": [
-                "0x954A00E7B0026",
-                "0x1459941A41402E"
-            ]}
-        df = pd.DataFrame(Data(output))
+    output = {
+    "Command": "get",
+    "Output": {
+        "0x954A00E7B0026": {
+            "name": "POST 001",
+            "wifiAddress": "c4:2a:d0:5f:ca:b0",
+            "serialNumber": "F9FZ5J5KMF3M"
+        },
+        "0x1459941A41402E": {
+            "name": "POST 002",
+            "wifiAddress": "4c:b9:10:97:e9:ed",
+            "serialNumber": "GG7FFKTQQ1GC"
+        },  
+        "0x1459FR41A41402E": {
+            "name": "POST 03",
+            "wifiAddress": "4c:b9:10:97:e9:ed",
+            "serialNumber": "GG7FFKTQQ1GC"
+        },
+        "Errors": {
+            "0x1459941A41402E": {},
+            "0x954A00E7B0026": {}
+        }
+    },
+    
+    "Type": "CommandOutput",
+    "Devices": [
+        "0x954A00E7B0026",
+        "0x1459941A41402E"
+    ]}
+    df = pd.DataFrame(Data(output))
+    addToDB=False
+    
+    if request.method == 'POST':
+        addToDB = bool(request.json["accept"])
+        print(addToDB)
+        df.to_sql(name='voyce_device', con=db.engine, index=False,if_exists= 'append')
+
+        return jsonify({'results': 'success'})
+    
+    if request.method == 'GET':
+        try:
         
-        # df.to_sql(name='voyce_device', con=db.engine, index=False,if_exists= 'append')
-        # col=[]
-        # added=[]
-        # for index in df.index:
-        #     col.append( f"Device {df['Device'][index]} at {df['Hospital'][index]}  has been added")
+            # cmd = 'cfgutil -f --format JSON get name serialNumber wifiAddress'
+            # result = subprocess.run(
+            #     cmd, shell=True, capture_output=True, text=True, check=True)
+            # output = json.loads(str(result.stdout))
+
+            # if addToDB:
+            #     print("triggered "+ str( addToDB))
             
-        #     added.append({"response" : col[index] })
-        return jsonify({"results": f"{len(df.index)} devices have been added"})
+            # df.to_sql(name='voyce_device', con=db.engine, index=False,if_exists= 'append')
+            # col=[]
+            # added=[]
+            # for index in df.index:
+            #     col.append( f"Device {df['Device'][index]} at {df['Hospital'][index]}  has been added")
+                
+            #     added.append({"response" : col[index] })
+            response= jsonify({"results": f"{len(df.index)} devices ready"})
+            response.headers.add('Access-Control-Allow-Origin', '*')
+            return response
 
 
-    except subprocess.CalledProcessError:
-        return jsonify({'error': 'nothing'})
+        except subprocess.CalledProcessError:
+            return jsonify({'results': 'nothing to add'})
+        
     
-    
-@app.route('/edit/<id>/', methods=['PUT'])
+@app.route('/edit/<id>/', methods=["PUT"])
 def edit(id):
     article = Voyce.query.get(id)
     
-    hospital = request.json["Hospital"]
-    device = request.json["Device"]
+    hospital = request.json["hospital"]
+    device = request.json["device"]
     
     article.Hospital = hospital
     article.Device = device
     db.session.commit()
-    
-    return voyceSchema.jsonify(article)
+    response =voyceSchema.jsonify(article)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
     
